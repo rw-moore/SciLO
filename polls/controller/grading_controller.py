53ca43fc8f41f8407b0bbf35e49c60b252938775
haotianzhu <haotianzhu@users.noreply.github.com>
2019-05-03 16:33:17 -0600

    feat: add grading system (#11)
    * feat: add grading system
    
    fix: add sql query
    
    fix: typo
    
    fix: question_attempt update
    
    * fix: remove log information

--- polls/controller/grading_controller.py
+++ polls/controller/grading_controller.py
@@ -1,15 +1,25 @@
 from django.shortcuts import get_object_or_404
 from polls.models import Response, ResponseAttempt
 from polls.serializers import AnswerSerializer
+<<<<<<< HEAD
 from polls.script.query.grading import get_responses_grades_by_questionattempt_id
 
 
 class GradingController:
+=======
+from django.db import connection
+from polls.script.query.grading import get_responses_grades_by_questionattempt_id
+
+class GradingController(object):
+>>>>>>> feat: add grading system (#11)
     '''
     A Management that given a response-attempt object, using sage script and saving result into response-attempt
     and then return it
     '''
+<<<<<<< HEAD
 
+=======
+>>>>>>> feat: add grading system (#11)
     def __init__(self, data):
         # {'answers_string': '2', 'response': '1', 'question_attempt': 1, 'id': '4'}
         self.data = data
@@ -29,6 +39,7 @@ class GradingController:
 <<<<<<< HEAD
         self._update_question_grade()
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
         a = self._update_question_grade()
         print(a,'htz')
@@ -39,8 +50,18 @@ class GradingController:
         return
 =======
 >>>>>>> add pylint
+=======
+>>>>>>> feat: add grading system (#11)
 
     def _grade_response(self):
+=======
+        return
+
+    def _grade_response(self):
+        # TODO grade policy
+        # skip
+        ###################
+>>>>>>> feat: add grading system (#11)
         self.grade = 0
         algorithm = self._find_its_algorithm()
         serializer = AnswerSerializer(self.response.answers, many=True)
@@ -49,6 +70,7 @@ class GradingController:
         for result in results:
             self.grade += result['accuracy']*self.response.weight
         return self.grade
+<<<<<<< HEAD
 
     def _update_response_grade(self):
         self.response_attempt.grade = self.grade
@@ -68,3 +90,30 @@ class GradingController:
         self.question_attempt.grade = self.question.weight*(response_total_grade/response_total_weight)
         self.question_attempt.save()
         return self.question_attempt.grade
+=======
+    
+    def _update_response_grade(self):
+        self.response_attempt.grade = self.grade
+        self.response_attempt.save()
+    
+    def _update_question_grade(self):
+        # rows is a list of (avg, max, min, id, weight)
+        rows = get_responses_grades_by_questionattempt_id(self.question_attempt.id)
+        # TODO grade policy
+        # skip
+        # for now, we just avg
+        ###################
+        response_total_weight = 0
+        response_total_grade = 0
+        for row in rows:
+            response_total_weight += row[4]
+            response_total_grade += row[0]
+        self.question_attempt.grade = self.question.weight*(response_total_grade/response_total_weight)
+        self.question_attempt.save()
+        return self.question_attempt.grade
+
+
+
+        
+
+>>>>>>> feat: add grading system (#11)
