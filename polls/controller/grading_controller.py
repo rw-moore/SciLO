ae2ed7f5b87f320da12669f04eef30bec8cfe205
haotianzhu <zhuhaotian19970215@gmail.com>
2019-05-06 17:12:19 -0600

    add pylint

--- polls/controller/grading_controller.py
+++ polls/controller/grading_controller.py
@@ -1,14 +1,15 @@
 from django.shortcuts import get_object_or_404
 from polls.models import Response, ResponseAttempt
 from polls.serializers import AnswerSerializer
-from django.db import connection
 from polls.script.query.grading import get_responses_grades_by_questionattempt_id
 
-class GradingController(object):
+
+class GradingController:
     '''
     A Management that given a response-attempt object, using sage script and saving result into response-attempt
     and then return it
     '''
+
     def __init__(self, data):
         # {'answers_string': '2', 'response': '1', 'question_attempt': 1, 'id': '4'}
         self.data = data
@@ -25,7 +26,6 @@ class GradingController(object):
         self._grade_response()
         self._update_response_grade()
         self._update_question_grade()
-        return
 
     def _grade_response(self):
         self.grade = 0
@@ -36,32 +36,22 @@ class GradingController(object):
         for result in results:
             self.grade += result['accuracy']*self.response.weight
         return self.grade
-    
+
     def _update_response_grade(self):
         self.response_attempt.grade = self.grade
         self.response_attempt.save()
-    
+
     def _update_question_grade(self):
         # rows is a list of (avg, max, min, id, weight)
         rows = get_responses_grades_by_questionattempt_id(self.question_attempt.id)
-        # TODO grade policy
-        # skip
-        # for now, we just avg
-        ###################
         response_total_weight = 0
         response_total_grade = 0
         query = self.question.responses.all()
         for row in rows:
-            gp = query.get(pk=row[3]).grade_policy
-            index = gp.POLICY_CHOICES_MAP[gp.policy]
+            g_p = query.get(pk=row[3]).grade_policy
+            index = g_p.POLICY_CHOICES_MAP[g_p.policy]
             response_total_weight += row[-1]
             response_total_grade += row[index]
         self.question_attempt.grade = self.question.weight*(response_total_grade/response_total_weight)
         self.question_attempt.save()
         return self.question_attempt.grade
-
-        
-
-
-        
-
