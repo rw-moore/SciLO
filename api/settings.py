b65698cb842ca4e79f3b811f6c7c5e1599d7ed3f
haotianzhu <zhuhaotian19970215@gmail.com>
2019-05-10 17:27:56 -0600

    test docker
    test
    
    edit version
    
    add postgre script
    
    t
    
    settings
    
    space
    
    1
    
    1
    
    1
    
    feat     (new feature)
    fix      (bug fix)
    refactor (refactoring production code)
    style    (formatting, missing semi colons, etc; no code change)
    docs     (changes to documentation)
    test     (adding or refactoring tests; no production code change)
    chore    (updating grunt tasks etc; no production code change)
    
    1
    
    1
    
    fix: conflict
    
    add database
    
    fix bugs
    
    fix
    
    fix
    
    fix

--- api/settings.py
+++ api/settings.py
@@ -86,8 +86,8 @@ WSGI_APPLICATION = 'api.wsgi.application'
 POSTGRES_HOST = os.environ.get("POSTGRES_HOST", '127.0.0.1')
 POSTGRES_PORT = os.environ.get("POSTGRES_PORT", '5432')
 POSTGRES_NAME = os.environ.get("POSTGRES_NAME", 'postgres')
-POSTGRES_USER = os.environ.get("POSTGRES_USER", '')
-POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD", '')
+POSTGRES_USER = os.environ.get("POSTGRES_USER", 'postgres_user')
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD", 'postgres_password')
 
 DATABASES = {
     'default': {
