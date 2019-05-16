b38a7921fd24c8b3c2973d1950bcbc709c158d34
Tianqi Wang <twang2@ualberta.ca>
2019-05-16 11:00:26 -0600

    install dependence

---
+++ frontend/config-overrides.js
@@ -0,0 +1,14 @@
+// see https://ant.design/docs/react/use-with-create-react-app
+
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+module.exports = override(
+    fixBabelImports('import', {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true,
+    }),
+    addLessLoader({
+        javascriptEnabled: true,
+        modifyVars: { '@primary-color': '#1DA57A' },
+    }),
+);
