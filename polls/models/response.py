f600007a3d64d455e1c34d4a5022185d10ede975
haotianzhu <zhuhaotian19970215@gmail.com>
2019-06-03 19:17:07 -0600

    rm response obj

--- polls/models/response.py
+++ polls/models/response.py
@@ -1,80 +1,11 @@
-import json
 from django.db import models
 from django.core.exceptions import ValidationError
+from django.contrib.postgres.fields import JSONField
 from .gradepolicy import GradePolicy, GradePolicyField
 from .algorithm import AlgorithmField, StringComparisonAlgorithm
 
-
-def response_base_generate(rtype):
-    name = rtype.pop('name', None)
-    return ResponseBase(name, **rtype)
-
-
-def response_base_parser(instance):
-    data = {}
-    (_, rytpe, args) = instance.deconstruct()
-    data['name'] = rytpe[0]
-    for k, v in args.items():
-        data[k] = v
-    return data
-
-
-class ResponseBase:
-    '''
-    ResponseBase is corresponse to type attribute in Reponse model
-    ResponseBase has following attributes:
-    1. __response_type__, string name of response type
-    2. __args__: the map which keys are items
-
-    ResponseBase is able to be load from json or save to json by calling
-    save_to_json() or load_from_json()
-    '''
-    __types__ = ['string', 'mutiple_choice']
-
-    def __init__(self, name, **kwargs):
-        self.name = 'string'  # default
-        if name.lower() in self.__types__:
-            self.name = name.lower()
-        self.__args__ = {}
-        for k, v in kwargs.items():
-            self.__args__[k] = v
-
-    def deconstruct(self):
-        path = "polls.models.response.ResponseBase"
-        args = [self.name]
-        kwargs = self.__args__
-        return (path, args, kwargs)
-
-
-class ResponseField(models.Field):
-    '''
-    ResponseField is a Field of responseBase.
-    '''
-
-    description = "response field"
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-    def db_type(self, connection):
-        return 'TEXT'
-
-    def from_db_value(self, value, pression, connection):
-        if value is None:
-            return value
-        data = json.loads(value)
-        return response_base_generate(data)
-
-    def get_prep_value(self, value):
-        instance = value
-        if isinstance(value, ResponseBase):
-            instance = response_base_parser(instance)
-        return json.dumps(instance)
-
-    def deconstruct(self):
-        name, path, args, kwargs = super().deconstruct()
-        return name, path, args, kwargs
-
+def default_string_dict():
+    return {'name': 'string'}
 
 class Response(models.Model):
     '''
@@ -108,7 +39,7 @@ class Response(models.Model):
     mark = models.PositiveSmallIntegerField(default=100)
     algorithm = AlgorithmField(default=StringComparisonAlgorithm())
     grade_policy = GradePolicyField(default=GradePolicy(3, 0, 'average', 'int'))
-    rtype = ResponseField(default=ResponseBase('string'))
+    rtype = JSONField(default=default_string_dict)
 
     def __str__(self):
         return super().__str__()+' name: '+str(self.name)
