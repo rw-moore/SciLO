f93a7c8fe87802528016c06a15649896e2ebbb8e
Tianqi Wang <twang2@ualberta.ca>
2019-06-25 12:05:40 -0600

    CreateQuizForm

--- frontend/src/components/Forms/CreateQuizForm.js
+++ frontend/src/components/Forms/CreateQuizForm.js
@@ -1,7 +1,9 @@
-import {Button, Form, Input, DatePicker, Divider, Tooltip} from "antd";
+import {Button, Form, Input, DatePicker, Divider, Tooltip, Checkbox, Select, InputNumber, Col, Row} from "antd";
 import React from "react";
+import {Link} from "react-router-dom";
 
 const timeFormat = "YYYY-MM-DD HH:mm:ss";
+const notifyCondition = ["Deadline","Submission after deadline","Flag of a question","Every submission"];
 
 class CreateQuizForm extends React.Component {
 
@@ -22,7 +24,7 @@ class CreateQuizForm extends React.Component {
                     rangeTimeValue[0].format(timeFormat),
                     rangeTimeValue[1].format(timeFormat),
                 ],
-                'late-time': lateTimeValue.format(timeFormat)
+                'late-time': lateTimeValue ? lateTimeValue.format(timeFormat): undefined
             };
             console.log('Received values of form: ', values);
         });
@@ -45,6 +47,7 @@ class CreateQuizForm extends React.Component {
     render() {
         const TextArea = Input.TextArea;
         const { MonthPicker, RangePicker } = DatePicker;
+        const { Option, OptGroup } = Select;
 
         const formItemLayout = {
             labelCol: { span: 4 },
@@ -98,7 +101,88 @@ class CreateQuizForm extends React.Component {
                 </Form.Item>
                 <Divider dashed orientation="left">Questions</Divider>
                 <Divider dashed orientation="left">Settings</Divider>
-
+                <Form.Item
+                    label="Grading Policy"
+                    {...formItemLayout}
+                >
+                    <Row>
+                        <Col span={12}>
+                            <Form.Item
+                                label={<Tooltip title={"Leave EMPTY for unlimited attempts"}>Attempts</Tooltip>}
+                            >
+                                <InputNumber min={1} max={10} defaultValue={3}/>
+                            </Form.Item>
+                        </Col>
+                        <Col span={12}>
+                            <Form.Item
+                                label="Method"
+                            >
+                                <Select style={{ width: "50%" }} defaultValue="highest">
+                                    <Option value="highest">highest</Option>
+                                    <Option value="last">last attempt</Option>
+                                    <Option value="average">average</Option>
+                                    <Option value="minimum">minimum</Option>
+                                </Select>
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col span={12}>
+                            <Form.Item
+                                label="Deduction per attempt"
+                            >
+                                <InputNumber
+                                    defaultValue={0}
+                                    min={0}
+                                    max={100}
+                                    formatter={value => `${value}%`}
+                                    parser={value => value.replace('%', '')}
+                                />
+                            </Form.Item>
+                        </Col>
+                        <Col span={12}>
+                            <Form.Item
+                                label="Deduction after deadline:"
+                            >
+                                <InputNumber
+                                    disabled={!(this.props.form.getFieldValue("late-time"))}
+                                    defaultValue={20}
+                                    min={0}
+                                    max={100}
+                                    formatter={value => `${value}%`}
+                                    parser={value => value.replace('%', '')}
+                                />
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                </Form.Item>
+                <Form.Item
+                    label="Notify me when"
+                    {...formItemLayout}
+                >
+                    {getFieldDecorator('notify-condition', {
+                        initialValue: ["Deadline","Submission after deadline","Flag of a question"]
+                    })(
+                        <Checkbox.Group options={notifyCondition}/>
+                    )}
+                </Form.Item>
+                <Form.Item
+                    label="Who can edit"
+                    {...formItemLayout}
+                >
+                    {getFieldDecorator('editable', {
+                    })(
+                        <Select mode={"multiple"} style={{ width: "50%" }} >
+                            <OptGroup label="Professor">
+                                <Option value="jack">Jack</Option>
+                                <Option value="lucy">Lucy</Option>
+                            </OptGroup>
+                            <OptGroup label="TA">
+                                <Option value="Yiminghe">yiminghe</Option>
+                            </OptGroup>
+                        </Select>
+                    )}
+                </Form.Item>
                 <Button onClick={this.handleSubmit}/>
             </Form>
         );
