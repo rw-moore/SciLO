9d498dd56c4134c9d58a60d2311b0478ed85e92a
haotianzhu <zhuhaotian19970215@gmail.com>
2019-07-30 15:50:20 -0600

    fix permission

--- polls/views/quiz_views.py
+++ polls/views/quiz_views.py
@@ -94,11 +94,11 @@ class QuizViewSet(viewsets.ModelViewSet):
         Instantiates and returns the list of permissions that this view requires.
         """
         if self.action == 'create':
-            permissions = [IsAdminUser]
+            permissions = [IsAdminUser, IsAuthenticated]
         elif self.action == 'destroy':
-            permissions = [IsAdminUser]
+            permissions = [IsAdminUser, IsAuthenticated]
         elif self.action == 'list':
-            permissions = [IsAdminUser]
+            permissions = [IsAdminUser, IsAuthenticated]
         else:
             permissions = [IsAuthenticated]
         return [permission() for permission in permissions]
