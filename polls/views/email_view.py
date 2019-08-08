ef9e033ade6c439d15c8c75f5b418d5f5a8d7d0e
haotianzhu <zhuhaotian19970215@gmail.com>
2019-08-08 10:18:01 -0600

    send email with username and password

--- polls/views/email_view.py
+++ polls/views/email_view.py
@@ -14,6 +14,34 @@ class EmailCodeViewSet(viewsets.ModelViewSet):
     queryset = EmailCode.objects.all()
     serializer_class = EmailCodeSerializer
 
+    def send_email_code_without_auth(self, request, username):
+        password = request.GET.get('password', None)
+        if User.objects.filter(username=username).exists():
+            user = User.objects.get(username=username)
+            if user.check_password(password):
+                qs = EmailCode.objects.filter(author=user)
+                if qs.exists() and len(qs) == 1:
+                    qs[0].token = EmailCode.random_with_N_digits()
+                    qs[0].save()
+                    token = qs[0].token
+                else:
+                    ec = EmailCode.objects.create(author=user, token=EmailCode.random_with_N_digits())
+                    token = ec.token
+                if user.email:
+                    send_mail(
+                        'Hello',
+                        'Here is the message.\n' + 'token: ' + str(token),
+                        'moonbackreborn@gmail.com',
+                        [user.email],
+                        fail_silently=False,
+                    )
+                    return response.Response(status=200)
+                else:
+                    return response.Response(status=400, data={"message": "user does not have email"})
+
+        return response.Response(status=400, data={"message": "user does not have email"})
+
+
     def send_email_code(self, request):
         user = request.user
         qs = EmailCode.objects.filter(author=user)
@@ -66,6 +94,8 @@ class EmailCodeViewSet(viewsets.ModelViewSet):
         if self.action == 'send_email_code':
             permissions = [IsAuthenticated]
         elif self.action == 'validate_email_code':
+            permissions = [AllowAny]
+        elif self.action == 'send_email_code_without_auth':
             permissions = [AllowAny]
         else:
             permissions = [IsAdminUser]
