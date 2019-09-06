57f3281249fa3a994730175367ecd867732fd740
Tianqi Wang <twang2@ualberta.ca>
2019-09-06 14:20:03 -0600

    Question stats modal

--- frontend/src/components/QuestionPreviews/QuestionStatsCollapse.js
+++ frontend/src/components/QuestionPreviews/QuestionStatsCollapse.js
@@ -1,5 +1,30 @@
 import React from "react";
-import {Modal, Table} from "antd";
+import {Modal, Table, Descriptions, Card, Tag} from "antd";
+import moment from "../../pages/QuizList/TakeQuiz";
+import QuestionScoreTable from "../QuizCard/QuestionScoreTable";
+import theme from "../../config/theme";
+
+const DescriptionItem = ({ title, content }) => (
+    <div
+        style={{
+            fontSize: 14,
+            lineHeight: '8px',
+            marginBottom: 4,
+            color: 'rgba(0,0,0,0.65)',
+        }}
+    >
+        <p
+            style={{
+                marginRight: 8,
+                display: 'inline-block',
+                color: theme['@primary-color'],
+            }}
+        >
+            {title}:
+        </p>
+        {content}
+    </div>
+);
 
 export default class QuestionStatsCollapse extends React.Component {
 
@@ -47,7 +72,49 @@ export default class QuestionStatsCollapse extends React.Component {
             title: this.props.children,
             content: (
                 <div>
-                    <Table columns={columns} dataSource={data} size="small" pagination={false}/>
+
+                    <DescriptionItem title="Total Grade" content={`${this.props.question.grade} / ${this.props.question.mark}`}/>
+
+                    {this.props.question.responses.map((response, index) => {
+
+                        return (
+                            <Card
+                                bordered={false}
+                                title={index}
+                                size={"small"}
+                            >
+                                <DescriptionItem title="Grade Policy" content={
+                                    <div style={{marginLeft: 12}}>
+                                        <DescriptionItem title="Mark" content={response.mark}/>
+
+                                        {!!(response.grade_policy.penalty_per_try) &&
+                                        <DescriptionItem title="Penalty Per Try"
+                                                         content={response.grade_policy.penalty_per_try}/>}
+                                        {!!(response.grade_policy.free_tries) &&
+                                        <DescriptionItem title="Free Tries"
+                                                         content={response.grade_policy.free_tries}/>}
+                                        {!!(response.grade_policy.policy) &&
+                                        <DescriptionItem title="Policy"
+                                                         content={response.grade_policy.policy}/>}
+                                    </div>
+                                }
+                                 />
+                                <DescriptionItem title="Tries left" content={`${response.left_tries} / ${response.tries.length}`}/>
+                                {response.tries.map((attempt, index) => {
+                                    if (attempt[0]) {
+                                        return (
+                                            <div style={{color: attempt[2] ? "green" : "red"}}>
+                                                {`${index}. Answer: ${attempt[0]} Grade: ${attempt[1]}`}
+                                            </div>
+                                        )
+                                    }
+                                    else {
+                                        return <></>
+                                    }
+                                })}
+                            </Card>
+                        )
+                    })}
                 </div>
             ),
             width: "70%",
