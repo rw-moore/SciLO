cfe170a061e82e3d34a347a91ccbf03d9078823c
Tianqi Wang <twang2@ualberta.ca>
2019-10-16 13:42:41 -0600

    modular redesign

--- frontend/src/pages/Course/index.js
+++ frontend/src/pages/Course/index.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import {Link, withRouter} from "react-router-dom";
-import GetCourses from "../../networks/GetCourses";
 import {Button, Collapse, Icon, List, message, Typography} from "antd";
 import "./index.css";
 import GetCourseById from "../../networks/GetCourseById";
-import GetQuizByCourse from "../../networks/GetQuizByCourse";
-import OngoingQuiz from "../../components/QuizCard/OngoingQuiz";
-import moment from "moment";
-import GetAttemptListByQuiz from "../../networks/GetAttemptListByQuiz";
-import QuizInfoModal from "../../components/QuizCard/QuizInfoModal";
+import CourseQuizzes from "./CourseQuizzes";
 
 class Course extends React.Component {
     state = {
-        data: [],
         course: {},
-        quizzes: []
     };
 
     componentDidMount() {
@@ -34,45 +27,14 @@ class Course extends React.Component {
                 }
                 else {
                     const course = data.data;
-                    GetQuizByCourse(this.props.id, this.props.token).then(
-                        data => {
-                            if (!data || data.status !== 200) {
-                                message.error("Cannot fetch course quizzes, see console for more details.");
-                                this.setState({
-                                    fetching: false,
-                                })
-                            }
-                            else {
-                                this.setState({
-                                    fetching: false,
-                                    course: course,
-                                    quizzes: data.data
-                                })
-                            }
-                        }
-                    )
+                    this.setState({
+                        fetching: false,
+                        course: course,
+                    })
                 }
             }
         );
     };
-
-    fetchAttempt = (quizId, params = {}) => {
-        GetAttemptListByQuiz(quizId, this.props.token, params).then(data => {
-            if (!data || data.status !== 200) {
-                message.error("Cannot fetch quiz attempts, see console for more details.");
-            } else {
-                console.log(data.data);
-
-                this.setState({
-                    targetQuiz: quizId,
-                    quizAttempts: data.data.quiz_attempts,
-                    showQuizModal: true
-                });
-            }
-        });
-    };
-
-
 
     render() {
         const { Panel } = Collapse;
@@ -80,51 +42,7 @@ class Course extends React.Component {
             return (
                 <div className={"CoursePanel"}>
                     <Typography.Title level={2}>{`${this.state.course.shortname} - ${this.state.course.fullname}`}</Typography.Title>
-                    <Typography.Title level={3}>
-                        {`Quizzes`}
-                        <span style={{float: "right"}}>
-                            <Link to={{pathname: `/Quiz/new`, search: "?course="+this.state.course.id}}><Button type={"primary"} icon="plus" onClick={this.create}>Create a Quiz</Button></Link>
-                        </span>
-                    </Typography.Title>
-                    {
-                        // this.state.quizzes.map((quiz)=>{
-                        //     return (
-                        //         <OngoingQuiz
-                        //             action={this.fetchAttempt}
-                        //             id={quiz.id}
-                        //             title={<span style={{color: "red"}}>{quiz.title}</span>}
-                        //             status={quiz.status}
-                        //             endTime={moment.utc(quiz.late_time)}
-                        //             startTime={moment.utc(quiz.start_end_time[1])}
-                        //         />)
-                        // })
-
-                    }
-                    <List
-                        style={{maxHeight: "calc(100vh - 100px)", marginBottom: 24, overflowY:"auto"}}
-                        size={"small"}
-                        dataSource={this.state.quizzes}
-                        bordered
-                        className="listItem"
-                        pagination={{
-                            showSizeChanger: true,
-                            defaultPageSize: 20,
-                            pageSizeOptions: ['10','20','50','100']
-                        }}
-                        renderItem={item => (
-                            <List.Item actions={[
-                                <Icon type="bar-chart" />,
-                                <Link to={`/Quiz/edit/${item.id}`}><Icon type="edit" /></Link>,
-                                <Icon type="ellipsis" />]}
-                            >
-                                <List.Item.Meta
-                                    title={<Button type={"link"} onClick={()=>{this.fetchAttempt(item.id)}}>{item.title}</Button>}
-                                />
-                                <span>Status: {item.status}</span>
-                            </List.Item>
-                        )}
-                    />
-                    <QuizInfoModal token={this.props.token} id={this.state.targetQuiz} attempts={this.state.quizAttempts} visible={this.state.showQuizModal} onClose={()=>{this.setState({showQuizModal: false})}}/>
+                    {(!!this.state.course.id) && <CourseQuizzes course={this.state.course} token={this.props.token}/>}
                     <Typography.Title level={3}>{`Questions`}</Typography.Title>
 
                     <Typography.Title level={3}>{`People`}</Typography.Title>
