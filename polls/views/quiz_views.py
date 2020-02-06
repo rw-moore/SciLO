507c7161c0d9351e54ff6af6885797b704d5b273
haotianzhu <zhuhaotian19970215@gmail.com>
2020-02-06 13:33:35 -0700

    fix permission for updating quiz

--- polls/views/quiz_views.py
+++ polls/views/quiz_views.py
@@ -131,6 +131,8 @@ def get_or_delete_a_quiz(request, quiz_id):
         serializer = QuizSerializer(quiz, context=context)
         return HttpResponse(status=200, data=serializer.data)
     elif request.method == 'PUT':
+        if not user.is_staff and not user.profile.is_instructor:  # if neither instructor or admin
+            return HttpResponse(status=403)
         validate_quiz_questions(course_id, data, user)
         serializer = QuizSerializer(quiz, data=request.data, partial=False)
         if serializer.is_valid():
@@ -139,6 +141,8 @@ def get_or_delete_a_quiz(request, quiz_id):
             return HttpResponse(status=400, data=serializer.errors)
         return HttpResponse(status=200, data=QuizSerializer(quiz).data)
     elif request.method == 'PATCH':
+        if not user.is_staff and not user.profile.is_instructor:  # if neither instructor or admin
+            return HttpResponse(status=403)
         validate_quiz_questions(course_id, data, user)
         serializer = QuizSerializer(quiz, data=request.data, partial=True)
         if serializer.is_valid():
