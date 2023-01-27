468bff7b745ff1375b791d7535189760c653ab74
Christopher Orlick <corlick@ualberta.ca>
2023-01-27 12:14:24 -0700

    cors 5

--- api/settings.py
+++ api/settings.py
@@ -64,11 +64,11 @@ CSRF_COOKIE_SECURE = True
 CSRF_COOKIE_DOMAIN = ".ualberta.ca"
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
+    # 'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
